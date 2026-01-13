33a87d73f50e059653635c7c6bb4ca402e438e02
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-13 11:50:34 -0600

    sound finishing touch

--- Player/player.gd
+++ Player/player.gd
@@ -17,10 +17,10 @@ func _process(delta: float) -> void:
 	if !transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y*delta*thrust)
-			if !sfx_boost.is_playing():
+			if  not sfx_boost.is_playing():
 				sfx_boost.play()
-			else:
-				sfx_boost.stop()
+		else:
+			sfx_boost.stop()
 		
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0,0,delta*torque_thrust))
@@ -42,12 +42,13 @@ func _on_body_entered(body: Node) -> void:
 
 func crash_sequence():
 	transitioning=true
-
+	sfx_death.play()
 	await get_tree().create_timer(1).timeout
 	print("KABOOM")
 	get_tree().reload_current_scene.call_deferred()
 
 func complete_level(next_lvl_file):
 	transitioning=true
+	sfx_win.play()
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file(next_lvl_file)
