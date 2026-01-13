e1ba373e0a218849a76fc86f3b9be2795dcb90e4
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-13 12:09:35 -0600

    Rocket Boosters

--- Player/player.gd
+++ Player/player.gd
@@ -7,6 +7,9 @@ var transitioning :=false
 @onready var sfx_death: AudioStreamPlayer = $SFXDeath
 @onready var sfx_win: AudioStreamPlayer = $SFXWin
 @onready var sfx_boost: AudioStreamPlayer3D = $SFXBoost
+@onready var main_booster: GPUParticles3D = $MainBooster
+@onready var right_booster: GPUParticles3D = $RightBooster
+@onready var left_booster: GPUParticles3D = $LeftBooster
 
 
 
@@ -17,16 +20,24 @@ func _process(delta: float) -> void:
 	if !transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y*delta*thrust)
+			main_booster.emitting=true
 			if  not sfx_boost.is_playing():
 				sfx_boost.play()
 		else:
 			sfx_boost.stop()
+			main_booster.emitting=false
 		
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0,0,delta*torque_thrust))
+			right_booster.emitting=true
+		else:
+			right_booster.emitting=false
 		
 		if Input.is_action_pressed("rotate_right"):
 			apply_torque(Vector3(0,0,-delta*torque_thrust))
+			left_booster.emitting=true
+		else:
+			left_booster.emitting=false
 
 func _on_body_entered(body: Node) -> void:
 	if not transitioning:
@@ -43,6 +54,9 @@ func _on_body_entered(body: Node) -> void:
 func crash_sequence():
 	transitioning=true
 	sfx_death.play()
+	sfx_boost.stop()
+	main_booster.emitting=false
+	right_booster.emitting=false
 	await get_tree().create_timer(1).timeout
 	print("KABOOM")
 	get_tree().reload_current_scene.call_deferred()
@@ -50,5 +64,8 @@ func crash_sequence():
 func complete_level(next_lvl_file):
 	transitioning=true
 	sfx_win.play()
+	sfx_boost.stop()
+	main_booster.emitting=false
+	right_booster.emitting=false
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file(next_lvl_file)
